2003a2f09a098214ef97d8b5802d10181f78fc85
ZatinMe <30550654+ZatinMe@users.noreply.github.com>
2017-10-20 01:29:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,37 +36,8 @@ def webhook():
 
 def processRequest(req):
     if req.get("result").get("action") == "action_1":
-        
-     """SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
-        CLIENT_SECRET = 'client_secret.json'
-        
-        store = file.Storage('storage.json')
-        creds = store.get()
-        if not creds or creds.invalid:
-            flow = client.flow_from_clientsecrets(CLIENT_SECRET, SCOPES)
-            creds = tools.run_flow(flow, store)
-        GMAIL = build('gmail', 'v1', http=creds.authorize(Http()))
-
-        threads = GMAIL.users().threads().list(userId='me').execute().get('threads', [])
-        subjectll = ''
-        for thread in threads:
-            tdata = GMAIL.users().threads().get(userId='me', id=thread['id']).execute()
-            nmsgs = len(tdata['messages'])
-
-            if nmsgs > 0:
-                msg = tdata['messages'][0]['payload']
-                subject = ''
-                for header in msg['headers']:
-                    if header['name'] == 'Subject':
-                        # print("if hello")
-                        subject = header['value']
-                        subjectll = subjectll + " " + subject
-                        break"""
-               
-                    
         speech = "::: Do oyu know::"
         print(speech)
-
         return {
             "speech": speech,
             "displayText": speech,

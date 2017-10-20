06701624494a9b3e1bbf13cb42e1561b3bf60087
ZatinMe <30550654+ZatinMe@users.noreply.github.com>
2017-10-20 11:46:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,7 @@ from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 
-SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
-CLIENT_SECRET ='client_secret.json'
+
 # Flask app should start in global layout
 app = Flask(__name__)
 
